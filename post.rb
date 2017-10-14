17d358f4fa963c7edde6c588bd681f1473e48230
Thamer26 <thamer.alhejailan1@gmail.com>
2017-10-14 20:53:22 +0400

    Bloccit Models assignment Checkpoint 15

---
+++ post.rb
@@ -0,0 +1,57 @@
+class Post < ActiveRecord::Base
+  belongs_to :topic
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
+  has_many :labelings, as: :labelable
+  has_many :labels, through: :labelings
+  has_many :favorites, dependent: :destroy
+
+  default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
+  scope :order_by_title, -> { order('title DESC') }
+  scope :order_by_reverse_created_at, -> { order('created_at ASC') }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
+  after_create :create_vote
+  after_create :create_favorite
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(post: self, value: 1)
+  end
+
+  def create_favorite
+    favorites.create(post: self, user: user)
+    send_favorite_emails
+  end
+
+  def send_favorite_emails
+    self.favorites.each do |favorite|
+      FavoriteMailer.new_post(favorite.user, self).deliver_now
+    end
+  end
+end
